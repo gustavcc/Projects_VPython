5b952ad79acf0dc03727b730f79dc7f2b51c58b8
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-02-28 21:32:44 -0300

    Update 1_queda_livre.py

--- queda livre/1_queda_livre.py
+++ queda livre/1_queda_livre.py
@@ -6,21 +6,19 @@ piso = box(pos=(0,-20,0),size=(25,0.5,25), color = color.orange)
 
 v = vector(0,0,0) #velocidade
 y = particula.pos #posicao
-g = vector(0,-10,0) #gravidade/aceleração
+g = vector(0,-10,0) #gravidade/aceleraï¿½ï¿½o
 dt = 0.01 #variacao do tempo
 
 while True:
     rate(100)
     v += g*dt
     y += v*dt + 0.5*g*dt**2
-           
     
     if particula.pos.y < (piso.pos.y + 1):
-           v.y = -v.y
-           while True:
-               rate(100)
-               v += -g*dt
-               y += v*dt + 0.5*g*dt**2
-               if particula.pos.y < vector(0,0,0):
-                   break
-           
+        v.y = -v.y
+        while True:
+            rate(100)
+            v += -g*dt
+            y += v*dt + 0.5*g*dt**2
+            if particula.pos.y < vector(0,0,0):
+                break
